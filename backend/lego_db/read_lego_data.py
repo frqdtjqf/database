2f67a6cfe434ca761a5c58bffcfe125423d94cf0
Fridtjof Pernack <fridtjof.pernack@gmail.com>
2026-01-12 03:07:49 +0100

    general reading

--- backend/lego_db/read_lego_data.py
+++ backend/lego_db/read_lego_data.py
@@ -1,38 +1,12 @@
-from backend.lego_db.lego_models import LegoPart, Weapon, WeaponSlot, TemplateMinifigure
-from backend.lego_db.csv_versions.get_csv_versions import get_csv_part_versions
+from backend.sql_api import DataBaseWrapper, Table
+from backend.lego_db.lego_models import LegoPart, TemplateMinifigure, Weapon, ActualMinifigure
+from backend.lego_db.model_converter import dbrecord_to_legomodel
 
-def read_lego_parts(data : list[dict[str, str]], version_name: str) -> list[LegoPart]:
-    version = get_csv_part_versions(version_name)
-    parts = []
-    for row in data:
-        id = row.get(version.id)
-        color = row.get(version.color)
-        description = row.get(version.description, "")
-        
-        part = LegoPart(
-            id=id,
-            color=color,
-            description=description
-        )
-        parts.append(part)
-    return parts
+def read_models_from_table(db: DataBaseWrapper, table: Table) -> list[LegoPart|TemplateMinifigure|ActualMinifigure|Weapon]:
+    models = []
+    records = db.get_records(table)
 
-def read_template_minifigures(data : list[dict[str, str]], version_name: str) -> list[TemplateMinifigure]:
-    version = get_csv_part_versions(version_name)
-    minifigures = []
-    for row in data:
-        id = row.get(version.id)
-        name = row.get(version.name)
-        year = int(row.get(version.year))
-        sets = frozenset(row.get(version.sets).split(version.seperator))
-        description = row.get(version.description, "")
-
-        minifigure = TemplateMinifigure(
-            id=id,
-            name=name,
-            year=year,
-            sets=sets,
-            description=description
-        )
-        minifigures.append(minifigure)
-    return minifigures
+    for record in records:
+        model = dbrecord_to_legomodel(record, table)
+        models.append(model)
+    return models
